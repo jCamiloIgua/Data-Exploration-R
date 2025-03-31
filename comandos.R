5ee64f1c71e87ffb2573d441b54a7b6e22fabbde
Jonatan Camilo Igua Contreras <azz7542@gmail.com>
2025-03-31 15:06:04 -0500

    Upload file

---
+++ comandos.R
@@ -0,0 +1,20 @@
+#paquetes 
+install.packages('ggplot2')
+install.packages("reshape2")
+install.packages("knitr")
+install.packages("kableExtra") 
+install.packages("tidyr")
+install.packages("dplyr")
+install.packages("png") #imagenes
+install.packages("grid") # ver imagenes en R
+install.packages("png")
+
+library(ggplot2)
+library(reshape2) 
+library(knitr) 
+library(kableExtra) 
+library(tidyr)
+library(dplyr)
+library(png)
+library(grid)
+library(png)
